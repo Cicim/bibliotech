8dc2880e9a5aaacdf7abe972d4c965054dab8d67
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-03-25 16:20:49 +0100

    Inserimento dei primi script SQL

---
+++ sql/creazione-tabelle.sql
@@ -0,0 +1,340 @@
+-- -----------------------------------------------------
+-- Schema Biblioteca
+-- -----------------------------------------------------
+DROP DATABASE IF EXISTS Biblioteca;
+CREATE DATABASE Biblioteca DEFAULT CHARACTER SET utf8;
+USE Biblioteca;
+
+-- -----------------------------------------------------
+-- Table Generi
+-- -----------------------------------------------------
+CREATE TABLE Generi (
+  idGenere INT NOT NULL,
+  Descrizione VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idGenere)
+);
+
+
+-- -----------------------------------------------------
+-- Table Tipologie
+-- -----------------------------------------------------
+CREATE TABLE Tipologie (
+  idTipologia INT NOT NULL,
+  Descrizione VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idTipologia)
+);
+
+
+-- -----------------------------------------------------
+-- Table Editori
+-- -----------------------------------------------------
+CREATE TABLE Editori (
+  idEditore INT NOT NULL,
+  Nome VARCHAR(45) NOT NULL,
+  Descrizione VARCHAR(200) NULL,
+  PRIMARY KEY (idEditore)
+);
+
+
+-- -----------------------------------------------------
+-- Table Collane
+-- -----------------------------------------------------
+CREATE TABLE Collane (
+  idCollana INT NOT NULL,
+  Nome VARCHAR(80) NOT NULL,
+  PRIMARY KEY (idCollana)
+);
+
+
+-- -----------------------------------------------------
+-- Table Lingue
+-- -----------------------------------------------------
+CREATE TABLE Lingue (
+  idLingua INT NOT NULL,
+  Descrizione VARCHAR(20) NOT NULL,
+  Abbreviazione VARCHAR(3) NOT NULL,
+  PRIMARY KEY (idLingua)
+);
+
+
+-- -----------------------------------------------------
+-- Table Libri
+-- -----------------------------------------------------
+CREATE TABLE Libri (
+  ISBN CHAR(13) NOT NULL,
+  Titolo VARCHAR(80) NOT NULL,
+  Descrizione VARCHAR(200) NULL,
+  AnnoPubblicazione INT NULL,
+  DataAggiunta DATE NOT NULL,
+  idGenere INT NOT NULL,
+  idTipo INT NOT NULL,
+  idEditore INT NOT NULL,
+  idCollana INT NULL,
+  idLingua INT NOT NULL,
+  PRIMARY KEY (ISBN),
+  FOREIGN KEY (idGenere) REFERENCES Generi(idGenere),
+  FOREIGN KEY (idTipo) REFERENCES Tipologie(idTipologia),
+  FOREIGN KEY (idEditore) REFERENCES Editori(idEditore),
+  FOREIGN KEY (idCollana) REFERENCES Collane(idCollana),
+  FOREIGN KEY (idLingua) REFERENCES Lingue(idLingua)
+);
+
+
+-- -----------------------------------------------------
+-- Table Nazionalità
+-- -----------------------------------------------------
+CREATE TABLE Nazionalità (
+  idNazionalità INT NOT NULL,
+  Descrizione VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idNazionalità)
+);
+
+
+-- -----------------------------------------------------
+-- Table Città
+-- -----------------------------------------------------
+CREATE TABLE Città (
+  idCittà INT NOT NULL AUTO_INCREMENT,
+  Nome VARCHAR(45) NOT NULL,
+  Provincia CHAR(2) NOT NULL,
+  PRIMARY KEY (idCittà)
+);
+
+
+-- -----------------------------------------------------
+-- Table Autori
+-- -----------------------------------------------------
+CREATE TABLE Autori (
+  idAutore INT NOT NULL,
+  NomeAutore VARCHAR(45) NOT NULL,
+  CognomeAutore VARCHAR(45) NOT NULL,
+  DataNascita DATE NOT NULL,
+  DataMorte DATE NULL,
+  Descrizione VARCHAR(200) NULL,
+  NomeArte VARCHAR(45) NULL,
+  idNazionalità INT NOT NULL,
+  idCittàNascita INT NOT NULL,
+  idCittàMorte INT NULL,
+  PRIMARY KEY (idAutore),
+  FOREIGN KEY (idNazionalità) REFERENCES Nazionalità(idNazionalità),
+  FOREIGN KEY (idCittàNascita) REFERENCES Città(idCittà),
+  FOREIGN KEY (idCittàMorte) REFERENCES Città(idCittà)
+);
+
+
+-- -----------------------------------------------------
+-- Table Autori_has_Libri
+-- -----------------------------------------------------
+CREATE TABLE Autori_has_Libri (
+  Autori_idAutore INT NOT NULL,
+  Libri_ISBN CHAR(13) NOT NULL,
+  Libri_codGenere INT NOT NULL,
+  Libri_codTipo INT NOT NULL,
+  Libri_codEditore INT NOT NULL,
+  PRIMARY KEY (Autori_idAutore, Libri_ISBN),
+  FOREIGN KEY (Autori_idAutore) REFERENCES Autori(idAutore),
+  FOREIGN KEY (Libri_ISBN) REFERENCES Libri(ISBN)
+);
+
+
+-- -----------------------------------------------------
+-- Table Ruolo_Scrittura
+-- -----------------------------------------------------
+CREATE TABLE Ruolo_Scrittura (
+  idRuolo INT NOT NULL,
+  Descrizione VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idRuolo)
+);
+
+
+-- -----------------------------------------------------
+-- Table Autori_Libri
+-- -----------------------------------------------------
+CREATE TABLE Autori_Libri (
+  idAutore INT NOT NULL,
+  ISBNLibro CHAR(13) NOT NULL,
+  DataContribuzione DATE NULL,
+  idRuoloScrittura INT NOT NULL,
+  PRIMARY KEY (idAutore, ISBNLibro),
+  FOREIGN KEY (idAutore) REFERENCES Autori(idAutore),
+  FOREIGN KEY (ISBNLibro) REFERENCES Libri(ISBN),
+  FOREIGN KEY (idRuoloScrittura) REFERENCES Ruolo_Scrittura(idRuolo)
+);
+
+
+-- -----------------------------------------------------
+-- Table Enti
+-- -----------------------------------------------------
+CREATE TABLE Enti (
+  idEnte INT NOT NULL,
+  NomeEnte VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idEnte)
+);
+
+
+-- -----------------------------------------------------
+-- Table Biblioteche
+-- -----------------------------------------------------
+CREATE TABLE Biblioteche (
+  idBiblioteca INT NOT NULL,
+  Email VARCHAR(45) NOT NULL,
+  TelefonoFisso VARCHAR(15) NULL,
+  Principale TINYINT NOT NULL,
+  ViaPzz VARCHAR(45) NOT NULL,
+  NumeroCivico SMALLINT NULL,
+  Città INT NOT NULL,
+  SitoWeb VARCHAR(45) NULL,
+  NomeBiblioteca VARCHAR(45) NOT NULL,
+  EnteAppartenente INT NOT NULL,
+  PRIMARY KEY (idBiblioteca),
+  FOREIGN KEY (EnteAppartenente) REFERENCES Enti(idEnte),
+  FOREIGN KEY (Città) REFERENCES Città(idCittà)
+);
+
+
+-- -----------------------------------------------------
+-- Table Edifici
+-- -----------------------------------------------------
+CREATE TABLE Edifici (
+  idEdificio INT NOT NULL,
+  Descrizione VARCHAR(45) NULL,
+  idBiblioteca INT NOT NULL,
+  PRIMARY KEY (idEdificio),
+  FOREIGN KEY (idBiblioteca) REFERENCES Biblioteche(idBiblioteca)
+);
+
+
+-- -----------------------------------------------------
+-- Table Piani
+-- -----------------------------------------------------
+CREATE TABLE Piani (
+  idPiano INT NOT NULL,
+  Numero INT NOT NULL,
+  idEdificio INT NOT NULL,
+  PRIMARY KEY (idPiano),
+  FOREIGN KEY (idEdificio) REFERENCES Edifici(idEdificio)
+);
+
+
+-- -----------------------------------------------------
+-- Table Sezioni
+-- -----------------------------------------------------
+CREATE TABLE Sezioni (
+  idSezione INT NOT NULL AUTO_INCREMENT,
+  Descrizione VARCHAR(45) NULL,
+  idPiano INT NOT NULL,
+  PRIMARY KEY (idSezione),
+  FOREIGN KEY (idPiano) REFERENCES Piani(idPiano)
+);
+
+
+-- -----------------------------------------------------
+-- Table Armadi
+-- -----------------------------------------------------
+CREATE TABLE Armadi (
+  idArmadio INT NOT NULL AUTO_INCREMENT,
+  Descrizione VARCHAR(45) NOT NULL,
+  idScaffale INT NOT NULL,
+  PRIMARY KEY (idArmadio),
+  FOREIGN KEY (idScaffale) REFERENCES Sezioni(idSezione)
+);
+
+
+-- -----------------------------------------------------
+-- Table Ripiani
+-- -----------------------------------------------------
+CREATE TABLE Ripiani (
+  idRipiano INT NOT NULL AUTO_INCREMENT,
+  idArmadio INT NOT NULL,
+  NumeroRipiano INT NOT NULL,
+  PRIMARY KEY (idRipiano),
+  FOREIGN KEY (idArmadio) REFERENCES Armadi(idArmadio)
+);
+
+
+-- -----------------------------------------------------
+-- Table Concessioni
+-- -----------------------------------------------------
+CREATE TABLE Concessioni (
+  idConcessioni INT NOT NULL,
+  DataPrestito DATE NOT NULL,
+  DataConsegna DATE NULL,
+  DataScadenza DATE NOT NULL,
+  idBiblioteca INT NOT NULL,
+  PRIMARY KEY (idConcessioni),
+  FOREIGN KEY (idBiblioteca) REFERENCES Biblioteche(idBiblioteca)
+);
+
+
+-- -----------------------------------------------------
+-- Table Copie
+-- -----------------------------------------------------
+CREATE TABLE Copie (
+  idCopia INT NOT NULL AUTO_INCREMENT,
+  Prestato TINYINT NOT NULL,
+  ISBN CHAR(13) NOT NULL,
+  idRipiano INT NOT NULL,
+  ProprietàDi INT NULL,
+  PRIMARY KEY (idCopia),
+  FOREIGN KEY (ISBN) REFERENCES Libri(ISBN),
+  FOREIGN KEY (idRipiano) REFERENCES Ripiani(idRipiano),
+  FOREIGN KEY (ProprietàDi) REFERENCES Concessioni(idConcessioni)
+);
+
+
+-- -----------------------------------------------------
+-- Table Utenti
+-- -----------------------------------------------------
+CREATE TABLE Utenti (
+  CodFiscale CHAR(16) NOT NULL,
+  Nome VARCHAR(45) NOT NULL,
+  Cognome VARCHAR(45) NOT NULL,
+  Email VARCHAR(45) NOT NULL,
+  ViaPzz VARCHAR(45) NOT NULL,
+  NumeroCivico SMALLINT NOT NULL,
+  TelefonoCellulare VARCHAR(15) NULL,
+  TelefonoCasa VARCHAR(15) NULL,
+  Validato TINYINT NOT NULL,
+  CodiceValidazione VARCHAR(45) NULL,
+  DataValidazione DATE NULL,
+  Sesso CHAR(1) NOT NULL,
+  Password INT NOT NULL,
+  Città INT NOT NULL,
+  DataNascita DATE NOT NULL,
+  Permessi TINYINT NOT NULL DEFAULT 3,
+  PRIMARY KEY (CodFiscale),
+  FOREIGN KEY (Città) REFERENCES Città(idCittà)
+);
+
+
+-- -----------------------------------------------------
+-- Table Prestiti
+-- -----------------------------------------------------
+CREATE TABLE Prestiti (
+  idPrestito INT NOT NULL,
+  DataConsegna DATETIME NOT NULL,
+  DataRiconsegna DATETIME NULL,
+  idCopia INT NOT NULL,
+  codFiscaleUtente CHAR(16) NOT NULL,
+  bibConsenga CHAR(16) NOT NULL,
+  bibRiconsegna CHAR(16) NULL,
+  PRIMARY KEY (idPrestito, bibRiconsegna),
+  FOREIGN KEY (idCopia) REFERENCES Copie(idCopia),
+  FOREIGN KEY (codFiscaleUtente) REFERENCES Utenti(CodFiscale),
+  FOREIGN KEY (bibConsenga) REFERENCES Utenti(CodFiscale),
+  FOREIGN KEY (bibRiconsegna) REFERENCES Utenti(CodFiscale)
+);
+
+
+-- -----------------------------------------------------
+-- Table Lista_Interessi
+-- -----------------------------------------------------
+CREATE TABLE Lista_Interessi (
+  codFiscaleUtente CHAR(16) NOT NULL,
+  ISBNLibro CHAR(13) NOT NULL,
+  Commento VARCHAR(45) NULL,
+  DataInserimento DATE NOT NULL,
+  PRIMARY KEY (codFiscaleUtente, ISBNLibro),
+  FOREIGN KEY (codFiscaleUtente) REFERENCES Utenti(CodFiscale),
+  FOREIGN KEY (ISBNLibro) REFERENCES Libri(ISBN)
+);
