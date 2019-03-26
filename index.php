a4bf058e267c302c4aaa22dd2cbc27e93c341843
Cici HP <claudio.ccmrr@gmail.com>
2019-03-26 18:50:23 +0100

    Creata l'homepage

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- File creato inizialmente da Claudio Cicimurri -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Homepage - Bibliotech</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    
+</body>
+</html>
