51a9253bd5df65586a9bcf0567e6b30f17846296
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-03-27 13:29:32 +0100

    Aggiunto esempio vetrina

--- index.php
+++ index.php
@@ -9,10 +9,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <?php include "php/imports.php"; ?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/catalogo.css">
 </head>
 
 <body>
     <!-- Pagina dell'header importata -->
     <?php include "php/header.php"; ?>
     <!-- Homepage - Vetrina -->
+    <div class="container">
+    <h3 class="h3">Libri in Evidenza</h3>
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="https://imgc.allpostersimages.com/img/print/u-g-F8PQ9I0.jpg?w=550&h=550&p=0">
+                    </a>
+                    <ul class="social">
+                        <li><a href="" data-tip="Occhiata Veloce"><i class="fa fa-search" style="font-size:36px"></i></a></li>
+                        <li><a href="" data-tip="Aggiungi a Lista Desideri"><i class="fa fa-shopping-bag " style="font-size:36px"></i></a></li>
+                        <li><a href="" data-tip="Aggiungi al Carrello"><i class="fa fa-shopping-cart" style="font-size:36px"></i></a></li>
+                    </ul>
+                </div>
+                <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star disable"></li>
+                </ul>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Libro Harry Potter</a></h3>
+                    <a class="add-to-cart" href="">Aggiungi</a>
+                </div>
+            </div>
+        </div>
+	</div>
+</div>
 </body>
