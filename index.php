5175a68e809f9e35831df65656f04940a3468260
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-03-27 13:30:07 +0100

    Inserito il footer

--- index.php
+++ index.php
@@ -18,33 +18,37 @@
     <?php include "php/header.php"; ?>
     <!-- Homepage - Vetrina -->
     <div class="container">
-    <h3 class="h3">Libri in Evidenza</h3>
-    <div class="row">
-        <div class="col-md-3 col-sm-6">
-            <div class="product-grid">
-                <div class="product-image">
-                    <a href="#">
-                        <img class="pic-1" src="https://imgc.allpostersimages.com/img/print/u-g-F8PQ9I0.jpg?w=550&h=550&p=0">
-                    </a>
-                    <ul class="social">
-                        <li><a href="" data-tip="Occhiata Veloce"><i class="fa fa-search" style="font-size:36px"></i></a></li>
-                        <li><a href="" data-tip="Aggiungi a Lista Desideri"><i class="fa fa-shopping-bag " style="font-size:36px"></i></a></li>
-                        <li><a href="" data-tip="Aggiungi al Carrello"><i class="fa fa-shopping-cart" style="font-size:36px"></i></a></li>
+        <h3 class="h3">Libri in Evidenza</h3>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid">
+                    <div class="product-image">
+                        <a href="#">
+                            <img class="pic-1" src="https://imgc.allpostersimages.com/img/print/u-g-F8PQ9I0.jpg?w=550&h=550&p=0">
+                        </a>
+                        <ul class="social">
+                            <li><a href="" data-tip="Occhiata Veloce"><i class="fa fa-search" style="font-size:36px"></i></a></li>
+                            <li><a href="" data-tip="Aggiungi a Lista Desideri"><i class="fa fa-shopping-bag " style="font-size:36px"></i></a></li>
+                            <li><a href="" data-tip="Aggiungi al Carrello"><i class="fa fa-shopping-cart" style="font-size:36px"></i></a></li>
+                        </ul>
+                    </div>
+                    <ul class="rating">
+                        <li class="fa fa-star"></li>
+                        <li class="fa fa-star"></li>
+                        <li class="fa fa-star"></li>
+                        <li class="fa fa-star"></li>
+                        <li class="fa fa-star disable"></li>
                     </ul>
-                </div>
-                <ul class="rating">
-                    <li class="fa fa-star"></li>
-                    <li class="fa fa-star"></li>
-                    <li class="fa fa-star"></li>
-                    <li class="fa fa-star"></li>
-                    <li class="fa fa-star disable"></li>
-                </ul>
-                <div class="product-content">
-                    <h3 class="title"><a href="#">Libro Harry Potter</a></h3>
-                    <a class="add-to-cart" href="">Aggiungi</a>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">Libro Harry Potter</a></h3>
+                        <a class="add-to-cart" href="">Aggiungi</a>
+                    </div>
                 </div>
             </div>
         </div>
-	</div>
-</div>
-</body>
+    </div>
+
+    <!-- Pagina del footer importata -->
+    <?php include "php/footer.php"; ?>
+
+</body> 
---
+++ php/footer.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="css/footer.css">
+<footer class="footer bg-dark text-light">
+    <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    Bibliotech è stato sviluppato dalle classi quinte B e C Informatico dell'Istituto d'Istruzione Superiore Carlo e Nello Rosselli di Aprilia (LT)
+                </div>
+                <div class="col-sm">
+                    <a href="#" class="text-light">Termini e condizioni</a><br>
+                    <a href="#" class="text-light">Uso dei Cookies</a>
+                </div>
+                <div class="col-sm">
+                    <a href="#" class="text-light">Chi siamo</a><br>
+                    <a href="#" class="text-light">Contattaci</a>
+                </div>
+            </div>
+            <hr>
+            @ Copyright <?php echo date("Y") ?>
+        </div>
+    </div>
+</footer> 
