c6051bae0a3afaa836df25f403e668fc2c8e7711
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-03-27 09:36:16 +0100

    Seperato l'header

--- index.php
+++ index.php
@@ -14,26 +14,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-success">
-    <a class="navbar-brand" href="#">Bibliotech</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Catalogo</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Cerca nel catalogo">
-            <button class="btn btn-light my-2 my-sm-0" type="submit">Cerca</button>
-        </form>
-    </div>
-    </nav>
+    <?php
+        include "php/header.php";
+    ?>
 </body>
 </html>
