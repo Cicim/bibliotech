6938c18576d5e539c8598c6fdd4a4a63919fd55c
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-03-27 13:28:15 +0100

    Separati gli import nell'index.php

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <!-- File creato inizialmente da Claudio Cicimurri -->
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Homepage - Bibliotech</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet"
-     href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <?php include "php/imports.php"; ?>
 </head>
+
 <body>
-    <?php
-        include "php/header.php";
-    ?>
+    <!-- Pagina dell'header importata -->
+    <?php include "php/header.php"; ?>
+    <!-- Homepage - Vetrina -->
 </body>
-</html>
