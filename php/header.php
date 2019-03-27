9e4cabd934285f35e27066689666d5fd87e46af7
Cici HP <claudio.ccmrr@gmail.com>
2019-03-27 14:00:31 +0100

    Rimosso il dropdown per l'accesso

--- php/header.php
+++ php/header.php
@@ -13,23 +13,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Catalogo</a>
                 </li>
-
-                <div class="dropdown">
-                    <button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown">Accedi
-                        <span class="caret"></span></button>
-                    <ul class="dropdown-menu px-3" style="width: 20em">
-                        <form action="php/controllalogin.php" method="post">
-                            <span >Email:</span>
-                            <input class="form-control" type="text" name="email" size="40" />
-                            <span>Password:</span>
-                            <input class="form-control" type="password" name="password" size="40" /><br />
-                            <div class="text-center">
-                                <input type="submit" class="btn btn-info w-100" name="invio" value="Accedi" />
-                            </div>
-                        </form>
-                    </ul>
-                </div>
-
             </ul>
             <a class="btn btn-info ml-2" href="login.php">Accedi</a>
         </div>
