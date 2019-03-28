034af3a1b5556487eb50b43192d36a448c7008f5
Claziero <lorenzoclazzer.2000@outlook.it>
2019-03-28 18:38:07 +0100

    git commit -am "Modificato registrazione.php"

--- php/registrazione.php
+++ php/registrazione.php
@@ -161,6 +161,46 @@
         <p class="mt-5 mb-3 text-muted">&copy; Bibliotech, 2019 </p>
     </form>
 
+    <!--Script php per la memorizzazione dei dati sul database-->
+    <?php
+        //recupero dati inseriti nel form
+        if(!isset($_POST["nome"]))
+            echo "Inserisci i dati prima";
+        else {
+            $nome = $_POST["nome"];
+            $cognome = $_POST["cognome"];
+            $sesso = $_POST["sesso"];
+            $viaPzz = $_POST["viaPzz"];
+            $citta = $_POST["citta"];
+            $numeroCivico = $_POST["numeroCivico"];
+            $codFiscale = $_POST["codFiscale"];
+            $dataNascita = $_POST["datetimepicker"];
+            $telCellulare = $_POST["telCellulare"];
+            $telFisso = $_POST["telFisso"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+    
+            //variabili per la connessione al DB
+            $host = "localhost";
+            $user = "root";
+            $db = "Biblioteca";
+    
+            //connetto al DB
+            $conn = mysqli_connect($host, $user, "") or die("Impossibile raggiungere il DBMS");
+    
+            //connetto al database
+            $seldb = mysqli_select_db($conn, $db) or die("Impossibile connettersi al database");
+    
+            //definisco la query di inserimento
+            $qry = "INSERT INTO Utenti 
+                (CodFiscale, Nome, Cognome, Email, ViaPzz, NumeroCivico, TelefonoCellulare, TelefonoFisso, Validato, Sesso, Password, Città, DataNascita, Permessi) VALUES
+                (\"$codFiscale\", \"$nome\", \"$cognome\", \"$viaPzz\", \"$numeroCivico\", \"$telCellulare\", \"$telFisso\", 0, \"$sesso\", \"$password\", \"$citta\", \"$dataNascita\", 3)";
+    
+            //stampo gli indirizzi delle stazioni
+            $query_res = mysqli_query($conn, $qry) or die("Impossibile eseguire la query");
+        }
+    ?>
+
 </body>
 
 </html> 
