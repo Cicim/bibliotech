a4ef6ffac4949cc846d56a8f42e545d015e30793
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-03-28 13:29:57 +0100

    Sistemato lo stile del login

--- login.php
+++ login.php
@@ -1,26 +1,45 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 
 <head>
-	<title>Identificazione dell'utente</title>
-	
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Bibliotech - Login</title>
+
+    <!-- Bootstrap core CSS -->
     <?php include "php/imports.php" ?>
+
+    <!-- Custom styles for this template -->
+    <link href="css/login.css" rel="stylesheet">
 </head>
 
-<body>
+<body class="text-center">
+
     <?php include "php/header.php" ?>
-    <h2>Accesso all'area riservata</h2>
-    <form action="php/controllalogin.php" method="post">
-        <p>
-            Email: <input type="text" name="email" size="40" />
-            Password: <input type="password" name="password" size="40" /><br />
-        </p>
-        <p>
-            <input type="submit" name="invio" value="Login" />
-            <input type="reset" name="cancella" size="Annulla" />
-        </p>
+
+    <form class="form-signin">
+        <!-- Icona di Bibliotech -->
+        <h1> Bibliotech </h1>
+        <h1 class="h3 mb-3 font-weight-normal">Schermata di accesso</h1> <br>
+
+        <label for="inputEmail" class="sr-only">Indirizzo mail</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Indirizzo mail" required autofocus>
+
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Ricordami
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-info btn-block" type="submit">Accedi</button> <br>
+
+        <a href="/bib2/php/recupero.php" class="text-info"> Recupero password </a> <br>
+        <a href="/bib2/php/registrazione.php" class="text-info"> Registrati </a>
+        <p class="mt-5 mb-3 text-muted">&copy; Bibliotech, 2019 </p>
     </form>
-    <?php include "php/footer.php" ?>
 </body>
 
-</html>
+</html> 
