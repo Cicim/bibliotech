cbe4e59331605177594504443d7e299a8df5a52f
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-03-28 13:30:37 +0100

    Sistemato l'header

--- php/header.php
+++ php/header.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark light bg-dark">
-        <a class="navbar-brand" href="#">Bibliotech</a>
+        <a class="navbar-brand" href="/bib2/index.php">Bibliotech</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,13 +8,21 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="/bib2/index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Catalogo</a>
                 </li>
             </ul>
-            <a class="btn btn-info ml-2" href="login.php">Accedi</a>
+
+            <!-- Bottone per poter visualizzare numero di libri della Lista Desideri. Al click va nella lista -->
+            <!--
+            <a class="btn btn-info ml-2" id="libriDesiderio" href="">
+                <i class="fa fa-book" style="font-size:24px;color:brown" aria-hidden="true"></i> Libri Desiderati
+            </a>
+            Non va qua
+            -->
+            <a class="btn btn-info ml-2" id="btnAccedi" href="login.php">Accedi</a>
         </div>
     </nav>
 </header> 
