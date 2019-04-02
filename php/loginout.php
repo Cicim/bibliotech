cc2505bd38251492ce86044039a2c045a8d5f74e
Claziero <lorenzoclazzer.2000@outlook.it>
2019-04-02 19:30:47 +0200

    eliminato loginok e loginout

--- php/loginout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-// elimina le variabili di sessione impostate
-$_SESSION = array();
-// elimina la sessione
-session_destroy();
-echo "Disconnessione riuscita, arrivederci!";
-
-?>
