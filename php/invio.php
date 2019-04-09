32699e1807a379d7d8dffe43e24ba4f8b60566e0
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-04-09 20:23:59 +0200

    Invio mail al recupero password

--- php/invio.php
+++ php/invio.php
@@ -3,6 +3,11 @@
 require_once "libs/class.phpmailer.php";
 require_once "libs/class.smtp.php";
 
+// Dichiara l'URL del sito come costante
+const URL_SITO = "localhost/bib2"; 
+// Dichiara la mail di contatti come costante
+const CONTATTO = "err@bib2.no";
+
 /**
  * @author Claudio Cicimurri, 5CI
  * Funzione per inviare una mail
@@ -58,15 +63,18 @@ function inviaMail($a, $oggetto, $html)
  * Funzione per richiedere la conferma dell'account da parte dell'indirizzo mail
  * @param string $indirizzo Destinatario
  * @param string $validazione Codice per la validazione
+ * @param string $nome Il nome del destinatario
+ * @param string $cognome Il cognome del destinatario
+ * @param string $sesso Il sesso del destinatario
  * 
  * @return bool Se la mail è stata iniviata
  */
 function inviaMailDiConferma($indirizzo, $validazione, $nome, $cognome, $sesso)
 {
     // Nome del sito
-    $sito = "localhost/bib2";
+    $sito = URL_SITO;
     // Contatti
-    $email = "err@nomail.co";
+    $email = CONTATTO;
     // Ottieni l'anno attuale
     $anno = date('Y');
 
@@ -77,5 +85,34 @@ function inviaMailDiConferma($indirizzo, $validazione, $nome, $cognome, $sesso)
         $indirizzo,
         'Conferma account Bibliotech',
         "<body style='border:0;margin:0;font-family:Helvetica'><div style='background:black;color:white;padding:4em'><h1>Ciao, $nome $cognome!<br>Grazie per esserti registrat$strsesso</h1>Per completare l'attivazione del tuo account clicca su questo pulsante:<br><div style='text-align:center'><a style='margin:1em;text-decoration:none;padding:1em 2em;background:white;display:inline-block;border-radius:1em;color:black;font-family:Segoe UI' href='$sito/php/validazione.php?codice=$validazione'>Attiva il tuo account</a></div></div><div style='background:grey;color:lightgray;padding:1em;text-align:center;font-size:11pt'>Se non hai creato tu quest'account, puoi ignorare questa e-mail.<br>Per qualsiasi informazione, contatta <a href='mailto:$email' style='color:#ddd;'>$email</a>, non rispondere a questa mail<br><a href='$sito/' style='color:#ddd'>Bibliotech</a>, $anno</div></body>"
+    );
+}
+
+
+/**
+ * @author Claudio Cicimurri, 5CI
+ * Funzione per inviare una mail di recupero per la password
+ * 
+ * @param string $indirizzo Destinatario
+ * @param string $codice Codice per reimposta-password.php
+ * @param string $nome Il nome del destinatario
+ * @param string $cognome Il cognome del destinatario
+ * 
+ * @return bool Se la mail è stata iniviata
+ */
+function inviaMailDiRecupero($indirizzo, $codice, $nome, $cognome)
+{
+    // Nome del sito
+    $sito = URL_SITO;
+    // Contatti
+    $email = CONTATTO;
+    // Ottieni l'anno attuale
+    $anno = date('Y');
+
+    // Invia mail al destinatario
+    return inviaMail(
+        $indirizzo,
+        'Conferma account Bibliotech',
+        "<body style='border:0;margin:0;font-family:Helvetica'><div style='background:black;color:white;padding:4em'><h1>Bentornato, $nome $cognome!<br>Segui le istruzioni per recuperare il tuo account.</h1>Per recuperare il tuo account, premi il pulsante e segui le istruzioni:<br><div style='text-align:center'><a style='margin:1em;text-decoration:none;padding:1em 2em;background:white;display:inline-block;border-radius:1em;color:black;font-family:Segoe UI' href='$sito/pages/reimposta-password.php?codice=$codice'>Reimposta la tua password</a></div></div><div style='background:grey;color:lightgray;padding:1em;text-align:center;font-size:11pt'>Non ti sei iscritto a Bibliotech? Contattaci a <a href='mailto:$email' style='color:#ddd;'>$email</a>.<br> Non rispondere direttamente a questa mail<br><a href='$sito/' style='color:#ddd'>Bibliotech</a>, $anno</div></body>"
     );
 }
