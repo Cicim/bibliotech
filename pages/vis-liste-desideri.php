77b13fe1b40ceb629133647893394c6a044c030a
AntonioDVRO <antonio.daverio@gmail.com>
2019-04-30 23:10:06 +0200

    Completo il meccanismo di selezione/visualizzazione della lista dei desideri di un utente a scelta

--- pages/vis-liste-desideri.php
+++ pages/vis-liste-desideri.php
@@ -33,69 +33,67 @@
         </div>
         <br>
 
-        <!-- Selezione utente di cui visualizzare la lista dei desideri -->
-        <form method = "GET" name="codfiscaleutente" id="codfiscaleutente" action = "vis-liste-desideri.php">
-
         <div align=center>
 
-            <select name = "nomeUtente">
+        <!-- Selezione utente di cui visualizzare la lista dei desideri -->
+        <form method = "POST" name="nomeUtente" id="nomeUtente" action = "vis-liste-desideri.php">
 
-                <?php
-                    include_once "../php/connessione.php";
-                    $conn = connettitiAlDb();
+                <select name="nomeUtente">
+                    <?php
+                        include_once "../php/connessione.php";
+                        $conn = connettitiAlDb();
 
-                    $query = "SELECT CodFiscale, Nome, Cognome FROM Utenti";
+                        $query = "SELECT CodFiscale, Nome, Cognome FROM Utenti";
 
-                    $qres = mysqli_query($conn, $query);
+                        $qres = mysqli_query($conn, $query);
 
-                    while ($row = mysqli_fetch_assoc($qres)){
-                        echo "<option value='" . $row['CodFiscale'] . "'>" . $row['Nome'] . " " . $row['Cognome'] . "</option>";
-                    }
+                        while ($row = mysqli_fetch_assoc($qres)){
+                            echo "<option value='" . $row['CodFiscale'] . "'>" . $row['Nome'] . " " . $row['Cognome'] . "</option>";
+                        }
+                    ?>
+                </select>
+                <input type="submit" value="Seleziona">
 
-                    $_POST['nomeUtente'] = $row['CodFiscale'];
-                ?>
-
-            </select>
+            </form>
 
         </div>
 
         <br>
 
-        </form>
+        <br>
 
-
-
-        <!-- Lista dei desideri dell'utente -->
+        <!-- Lista dei desideri dell'utente specificato -->
         <table border="2" align="center" class="table">
             <?php
-            $codfisc = $_POST['nomeUtente'];
-            echo $codfisc;
-            // Connettiti al database
-            include_once "../php/connessione.php";
-            $conn = connettitiAlDb();
+            if(isset($_POST['nomeUtente'])){
+                // Connettiti al database
+                include_once "../php/connessione.php";
+                $conn = connettitiAlDb();
 
-            $query = "SELECT *
-                        FROM Libri, Lista_Interessi
-                        WHERE Lista_Interessi.codFiscaleUtente = '" . $codfisc . "'" .
-                        "AND Libri.ISBN = Lista_Interessi.ISBNLibro";
+                $query = "SELECT *
+                            FROM Libri, Lista_Interessi
+                            WHERE Lista_Interessi.codFiscaleUtente = '" . $_POST['nomeUtente'] . "'" .
+                            "AND Libri.ISBN = Lista_Interessi.ISBNLibro";
 
-            $risultato = mysqli_query($conn, $query)
-                or die("Errore nella query" . mysqli_error($conn));
+                $risultato = mysqli_query($conn, $query)
+                    or die("Errore nella query" . mysqli_error($conn));
 
-            echo
-                "<tr>
-                    <th>
-                        Titolo
-                    </th>
-                    <th>
-                        ISBN
-                    </th>
-                </tr>";
-                    while ($riga = mysqli_fetch_assoc($risultato)) {
-                        echo
-                            '<th><td>' . $riga['Titolo'] . '</td><td>' . $riga['ISBN'] . '</td></th>';
-                    }
-
+                echo
+                    "<tr>
+                        <th>
+                            Titolo
+                        </th>
+                        <th>
+                            ISBN
+                        </th>
+                    </tr>";
+                while ($riga = mysqli_fetch_assoc($risultato)) {
+                    echo '<tr><td>' . $riga['Titolo'] . '</td><td>' . $riga['ISBN'] . '</td></tr>';
+                }
+            }
+            else{
+                echo "control";
+            }
 
             ?>
         </table>
