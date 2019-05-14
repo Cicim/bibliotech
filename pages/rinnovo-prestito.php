fd5f0a2703453c5222947885c5b8116b5c03cd41
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-05-14 17:48:55 +0200

    Rinnovo prestito ora funzionante

--- pages/rinnovo-prestito.php
+++ pages/rinnovo-prestito.php
@@ -31,186 +31,18 @@ livelloRichiesto(BIBLIOTECARIO); ?>
     <div>
         <!-- Titolo della pagina -->
         <div class="jumbotron" style="padding: 2rem 2rem">
-            <h1 class="display-5 text-center">Creazione di un prestito</h1>
+            <h1 class="display-5 text-center">Rinnova il prestito</h1>
         </div>
-
-        <?php
-        include_once "../php/connessione.php";
-        $fisc = '';
-        /**
-         * @author Claudio Cicimurri, 5CI
-         * Funzione per uscire in qualsiasi momento con un errore
-         * dalla selezione del codice fiscale
-         * @return bool Se ha trovato l'utente
-         */
-        function selezioneCodiceFiscale()
-        {
-            // Connettiti al database
-            $conn = connettitiAlDb();
-
-            // Ottieni il codice fiscale
-            global $fisc;
-            $fisc = $_GET['codiceFiscale'];
-
-
-            // Ottieni l'utente, se esiste
-            $ris = mysqli_query($conn, "SELECT Nome, Cognome, Email FROM Utenti WHERE CodFiscale = '$fisc'");
-
-            // Chiudi la connessione
-            mysqli_close($conn);
-
-            if (mysqli_num_rows($ris) == 1) {
-                $ris = mysqli_fetch_row($ris);
-                global $nome, $cognome, $email;
-                // Ottieni i dati
-                $nome = $ris[0];
-                $cognome = $ris[1];
-                $email = $ris[2];
-
-                // Imposta lo stato a ok
-                return true;
-            } else
-                return false;
-        }
-
-        $stato_fisc = "";
-        // Controlla se l'utente è già stato selezionato
-        if (isset($_GET['codiceFiscale']))
-            $stato_fisc = selezioneCodiceFiscale();
-
-        $copiePrestate = 0;
-        /**
-         * @author Claudio Cicimurri, 5CI
-         * Funzione per uscire in qualsiasi momento con un errore
-         * dalla creazione di un prestito
-         * @return string Codice di errore o ok
-         */
-        function creazionePrestito()
-        {
-            global $stato_fisc, $fisc, $copiePrestate;
-            // Assicurati che un utente sia stato selezionato
-            if (!$stato_fisc)
-                return "Nessun utente selezionato<br>Impossibile proseguire";
-
-            // Ottieni il codice fiscale dell'utente
-            $codUtente = $fisc;
-            // Ottieni il codice fiscale del bibliotecario
-            $codBib = $_SESSION['user_id'];
-
-            // Ottieni l'id della copia
-            $idCopia = $_POST['idCopia'];
-            // Ottieni la data di riconsegna
-            $dataRiconsegna = $_POST['dataRiconsegna'];
-
-            // Se l'idCopia non è definito
-            if ($idCopia == '')
-                return "Non è stata scelta nessuna copia<br>Impossibile proseguire";
-            // Se la dataRiconsegna non è definita
-            if ($dataRiconsegna == '')
-                return "Data riconsegna non valida<br>Impossibile proseguire";
-
-            // Ottieni la data odierna
-            $dataConsegna = date('Y-m-d');
-
-            // Connettiti al db
-            $conn = connettitiAlDb();
-
-            // Separa le copie per ,
-            $copie = explode(',', $idCopia);
-            // Elimina i duplicati
-            $copie = array_unique($copie);
-            
-            // Crea un log di errore
-            $log = '<ol>';
-
-            // Manda un errore se qualche valore non è un numero
-            foreach ($copie as $idCopia) {
-                // Controlla che tutti gli id copia siano validi
-                if (preg_match("/^\d+$/", $idCopia) == false)
-                    return "Id copia non valido: \"$idCopia\". Evita di inserirli manualmente<br>Impossibile proseguire";
-            }
-
-            
-            // Per ogni copia
-            foreach ($copie as $idCopia) {
-                // Controlla se non è già stata prenotata
-                if (mysqli_fetch_row(mysqli_query($conn, "SELECT Prestato FROM Copie WHERE idCopia = $idCopia"))[0])
-                {
-                    // Stampa l'errore a log
-                    $log .= "<li>Trovata copia già prestata ($idCopia)</li>";
-                    continue;
-                }
-
-                // Bloccala
-                mysqli_query($conn, "UPDATE Copie SET Prestato = TRUE WHERE idCopia = $idCopia");
-
-                // Esegui la query
-                $query = "INSERT INTO Prestiti (DataConsegna, DataRiconsegna, idCopia, codFiscaleUtente, bibConsegna)
-                            VALUES ('$dataConsegna', '$dataRiconsegna', $idCopia, '$codUtente', '$codBib')";
-                // Aggiungi il prestito
-                $ris = mysqli_query($conn, $query);
-
-                // Se il prestito non riesce
-                // P.S. Non dovrebbe avvenire mai quest'errore
-                if (!$ris)
-                {
-                    // Stampa l'errore a log
-                    $log .= "<li>Errore durante l'esecuzione della query ($idCopia)<br>"
-                     . mysqli_error($conn) . '</li>';
-                    continue;
-                }
-
-                // Incrementa il numero di copie correttamente prestate
-                $copiePrestate++;
-            }
-
-            // Se non ci sono errori
-            if ($log == '<ol>')
-                return "ok";
-            // Altrimenti
-            else {
-                // Chiudi il log
-                $log .= '</ol>';
-                // Aggiungi il numero di copie prestate
-                if ($copiePrestate == 1) $log .= "Prestata una copia";
-                else $log .= "Prestate $copiePrestate copie";
-                // Riportalo come errore
-                return $log;
-            }
-        }
-
-        $stato = "";
-        // Script per effettuare il prestito
-        if (isset($_POST['dataRiconsegna']))
-            $stato = creazionePrestito();
-        ?>
-
 
         <!-- Contenitore -->
         <div class="container">
-            <?php
-            global $stato, $copiePrestate;
-
-            // Se il prestito è stato effettuato con successo
-            if ($stato == "ok")
-                // Stampa il numero di copie prestate
-                echo '<div class="alert alert-success" role="alert">' .
-                    ($copiePrestate == 1 ? "Copia del libro prestata" : "Prestate $copiePrestate copie")
-                . '</div>';
-            else if ($stato != '') {
-                echo '<div class="alert alert-danger" role="alert">';
-                echo $stato;
-                echo '</div>';
-            }
-            ?>
-
             <!-- Rinnovo della data -->
             <form action="" method="POST">
 
                 <label for="DataRinnovata">Seleziona la nuova data di scadenza del prestito</label>
                 <div class="form-group">
                     <div class="input-group date" id="datetimepicker" data-target-input="nearest">
-                        <input type="text" class="form-control datetimepicker-input" id="DataRinnovata" name="DataRinnovata" data-target="#datetimepicker" value="<?php echo $riconsegna ?>" />
+                        <input type="text" class="form-control datetimepicker-input" id="DataRinnovata" name="DataRinnovata" data-target="#datetimepicker"/>
                         <div class="input-group-append" data-target="#datetimepicker" data-toggle="datetimepicker">
                             <div class="input-group-text">
                                 <i class="fa fa-calendar" style="font-size:24px"></i>
@@ -237,47 +69,23 @@ livelloRichiesto(BIBLIOTECARIO); ?>
             </form>
             <?php
 
-            if(isset($_GET['btnRinnova'])){
+            if (isset($_GET['idPrestito'])) {
 
-              echo $_GET['btnRinnova'];
-              
-              $host = "localhost";
-              $user = "root";
-              $password = "";
+                $id = $_GET['idPrestito'];
 
-              $conn = mysqli_connect($host, $user, $password);
-
-              if(!$conn){
-                  echo "Impossibile connettersi al database";
-              }
-              else{
-
-                  $sel = mysqli_select_db($conn, "Biblioteca");
-
-                  if(!$sel){
-                      echo "Database non trovato";
-                  }
-                  else{
-
-                    if(isset($_POST['DataRinnovata'])){
-
-                      $query = "UPDATE Prestiti
+                include_once "../php/connessione.php";
+                $conn = connettitiAlDb();
+                if (isset($_POST['DataRinnovata'])) {
+                    // Esegui la query per sostituire la data di riconsegna
+                    $query = "UPDATE Prestiti
                       SET DataRiconsegna = \"" . $_POST['DataRinnovata'] .
-                      "\" WHERE idPrestito = \"" . $_POST['idPrestito'] . "\"";
+                        "\" WHERE idPrestito = \"$id\"";
 
-                      echo $query;
+                    $risultato = mysqli_query($conn, $query);
 
-                      $risultato = mysqli_query($conn, $query);
-                      if(!$risultato){
-                          echo "Errore nella query";
-                      }
-                      else{
-
-                        
-                      
-                      }
+                    if (!$risultato) {
+                        echo "Errore nella query";
                     }
-                  }
                 }
             }
             ?>
--- pages/vis-prestiti-scadenze.php
+++ pages/vis-prestiti-scadenze.php
@@ -39,91 +39,72 @@ livelloRichiesto(BIBLIOTECARIO);
         <!-- Lista prestiti e scadenze -->
         <table align="center" class="table">
             <?php
-            # Definizione delle variabili di connessione al DBMS
-            $host = "localhost";
-            $user = "root";
-            $password = "";
-            # Effettua la connessione al DBMS
-            $conn = mysqli_connect($host, $user, $password);
-            # Controllo di eventuali errori
-            if (!$conn) {
-                echo "Impossibile connettersi al database";
-            }
-            # Effettua la selezione del database
-            else {
-                $sel = mysqli_select_db($conn, "Biblioteca");
-                # Controllo di eventuali errori
-                if (!$sel) {
-                    echo "Database non trovato";
-                }
-                # Esegue la query sul database
-                else {
-                    # Definizione della query
-                    # La query seleziona titolo, numero di copia, numero di prestito, data di presa in consegna, data di riconsegna prevista e generalità dell'utente dalle tabelle 'Libri', 'Utenti', 'Prestiti' e 'Copie'
-                    $query = "SELECT Libri.Titolo, Copie.idCopia, Prestiti.idPrestito, Prestiti.DataConsegna, Prestiti.DataRiconsegna, Utenti.Nome, Utenti.Cognome 
+            include_once "../php/connessione.php";
+            $conn = connettitiAlDb();
+            # Definizione della query
+            # La query seleziona titolo, numero di copia, numero di prestito, data di presa in consegna, data di riconsegna prevista e generalità dell'utente dalle tabelle 'Libri', 'Utenti', 'Prestiti' e 'Copie'
+            $query = "SELECT Libri.Titolo, Copie.idCopia, Prestiti.idPrestito, Prestiti.DataConsegna, Prestiti.DataRiconsegna, Utenti.Nome, Utenti.Cognome 
                     FROM Libri, Utenti, Prestiti, Copie
                     WHERE Libri.ISBN = Copie.ISBN
                     AND Utenti.CodFiscale = Prestiti.CodFiscaleUtente
                     AND Copie.idCopia = Prestiti.idCopia";
-                    # Esecuzione della query
-                    $risultato = mysqli_query($conn, $query);
-                    # Controllo di eventuali errori
-                    if (!$risultato) {
-                        echo "Errore nella query";
-                    } else {
-                        # Titoli delle colonne della tabella
-                        echo
-                            "<tr><th>" .
-                                "Titolo" .
-                                "</th><th>" .
-                                "Numero copia" .
-                                "</th><th>" .
-                                "Numero prestito" .
-                                "</th><th>" .
-                                "Data di consegna" .
-                                "</th><th>" .
-                                "Data di riconsegna" .
-                                "</th><th>" .
-                                "Nome utente" .
-                                "</th><th>" .
-                                "Cognome utente" .
-                                "</th><th>" .
-                                "Rinnovo prestito" .
-                                "</th><th>" .
-                                "Restituzione libro" .
-                                "</th></tr>";
-                        # Riempimento della tabella con i risultati della query
-                        # Ad ogni ripetizione corrisponde un record della tabella   
-                        while ($riga = mysqli_fetch_assoc($risultato)) {
-                            echo
-                                '<tr><td>' .
-                                    $riga['Titolo'] . '</td><td>' . $riga['idCopia'] . '</td><td>' . $riga['idPrestito'] . '</td><td>' . $riga['DataConsegna'] . '</td><td>' . $riga['DataRiconsegna'] . '</td><td>' . $riga['Nome'] . '</td><td>' . $riga['Cognome'];
-                            # Form per l'invio dei dati alla pagina di rinnovo prestito
-                            echo "<form method = \"GET\" action = \"rinnovo-prestito.php\">";
-                            # Pulsante per rinnovare il prestito
-                            echo "<td align=\"center\">
-                                <input type = \"hidden\" id = " . $riga['idPrestito'] . "name =" . $riga['idPrestito'] . "value=" . $riga['idPrestito'] . ">
-                                <button type = \"submit\" class=\"btn btn-info ml-2\">
+            # Esecuzione della query
+            $risultato = mysqli_query($conn, $query);
+            # Controllo di eventuali errori
+            if (!$risultato) {
+                echo "Errore nella query";
+            } else {
+                # Titoli delle colonne della tabella
+                echo
+                    "<tr><th>" .
+                        "Titolo" .
+                        "</th><th>" .
+                        "Numero copia" .
+                        "</th><th>" .
+                        "Numero prestito" .
+                        "</th><th>" .
+                        "Data di consegna" .
+                        "</th><th>" .
+                        "Data di riconsegna" .
+                        "</th><th>" .
+                        "Nome utente" .
+                        "</th><th>" .
+                        "Cognome utente" .
+                        "</th><th>" .
+                        "Rinnovo prestito" .
+                        "</th><th>" .
+                        "Restituzione libro" .
+                        "</th></tr>";
+                # Riempimento della tabella con i risultati della query
+                # Ad ogni ripetizione corrisponde un record della tabella   
+                while ($riga = mysqli_fetch_assoc($risultato)) {
+                    echo
+                        '<tr><td>' .
+                            $riga['Titolo'] . '</td><td>' . $riga['idCopia'] . '</td><td>' . $riga['idPrestito'] . '</td><td>' . $riga['DataConsegna'] . '</td><td>' . $riga['DataRiconsegna'] . '</td><td>' . $riga['Nome'] . '</td><td>' . $riga['Cognome'];
+                    # Form per l'invio dei dati alla pagina di rinnovo prestito
+                    # Pulsante per rinnovare il prestito
+                    echo "<td align=\"center\">";
+                    echo "<form method = \"GET\" action = \"rinnovo-prestito.php\">";
+                    echo "      <input type=\"hidden\" name=\"idPrestito\" value=" . $riga['idPrestito'] . ">
+                                <button type=\"submit\" class=\"btn btn-info ml-2\">
                                     <i class=\"fa fa-edit\"></i>Rinnova
                                 </button>
-                            </td></td>";
-                            $_GET['btnRinnova'] = $riga['idPrestito'];
-                            echo $_GET['btnRinnova'];
-                            echo "</form>";
+                            </td>";
+                    $_GET['btnRinnova'] = $riga['idPrestito'];
+                    echo $_GET['btnRinnova'];
+                    echo "</form>";
 
-                            # Form per l'invio dei dati alla pagina di restituzione del libro
-                            echo "<form method = \"GET\" action = \"restituzione-libro.php\">";
-                            # Pulsante per restituire il libro
-                            echo "<td align=\"center\">
+                    # Form per l'invio dei dati alla pagina di restituzione del libro
+                    echo "<form method = \"GET\" action = \"restituzione-libro.php\">";
+                    # Pulsante per restituire il libro
+                    echo "<td align=\"center\">
                                 <input type = \"hidden\" id = " . $riga['idPrestito'] . "name =" . $riga['idPrestito'] . "value=" . $riga['idPrestito'] . ">
                                 <button type = \"submit\" class=\"btn btn-info ml-2\" id=\"btnRestituzione\">
                                     <i class=\"fa fa-check\"></i>Restituzione
                                 </button>
                             </td></td>
                             </td></td>";
-                            echo "</form>";
-                        }
-                    }
+                    echo "</form>";
                 }
             }
             ?>
